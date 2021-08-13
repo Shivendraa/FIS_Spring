a92bdf32be31fe0e24ac024d3e44fde50d93d2e9
Shivendraa <kalseshivendra@gmail.com>
2021-08-13 13:13:16 +0530

    19 - Java8 Features - Lambda Expressions

---
+++ testing-web/src/test/java/com/fis/testingweb/SpringTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.testingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- testing-web/src/test/java/com/fis/testingweb/TestingWebApplicationTests.java
+++ testing-web/src/test/java/com/fis/testingweb/TestingWebApplicationTests.java
@@ -1,5 +1,4 @@
 package com.fis.testingweb;
-
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
